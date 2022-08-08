b6a88d1bf70fa2107927a1be8a9d0bce2c5ab780
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-08-08 21:20:40 +0530

    Added navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,12 +5,20 @@ export default function Navbar() {
     <div>
       <nav className="font-sans text-lg flex justify-between">
         <ul className="cursor-pointer flex justify-end">
-          <li className="hover:bg-slate-200 px-4 py-2">Home</li>
-          <li className="hover:bg-slate-200 px-4 py-2">About</li>
-          <li className="hover:bg-slate-200 px-4 py-2">Skills</li>
-          <li className="hover:bg-slate-200 px-4 py-2">Contact</li>
+          <li className="hover:bg-slate-200 px-6 py-4">Home</li>
+          <li className="hover:bg-slate-200 px-6 py-4">About</li>
+          <li className="hover:bg-slate-200 px-6 py-4">Skills</li>
+          <li className="hover:bg-slate-200 px-6 py-4">Experience</li>
+          <li className="hover:bg-slate-200 px-6 py-4">Education</li>
+          <li className="hover:bg-slate-200 px-6 py-4">Projects</li>
+          <li className="hover:bg-slate-200 px-6 py-4">Contact</li>
         </ul>
       </nav>
+      <div className="conataine px-14 py-16">
+        <p><h2 className="text-4xl py-1">Hello, This is</h2></p>
+        <p><h2 className="text-4xl py-1"></h2></p>
+        <p><h1 className="text-7xl py-1">Suryansh Singh Bisen</h1></p>
+      </div>
     </div>
   );
 }
