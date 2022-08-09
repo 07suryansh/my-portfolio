27d29c10e45e569648d7d2754e59ac324b0c3441
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-08-10 00:01:38 +0530

    Given heading to about

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,13 +8,14 @@ export default function About() {
         <img
             src={Me}
             alt=""
-            className="w-[30rem] rounded-2xl border-solid border-8 border-blue-500 bg-red-200 drop-shadow-2xl
+            className="w-[30rem] rounded-2xl border-solid border-[12px] border-blue-500 bg-red-200 drop-shadow-2xl
             transition ease-in-out delay-900  hover:-translate-y-1 hover:scale-110 duration-200 
             "
           />
         </div>
         <div className="container">
-        <h1>About</h1>
+        <h1 className='text-4xl border-blue-500 border-b-4 pb-2'>About</h1>
+        
         </div>
     </div>
   )
