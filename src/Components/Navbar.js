a67052ebfab1b62ff457fee394f32bcc06bdf8eb
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-08-09 22:16:13 +0530

    Added new components Home

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Me from "../Images/MySelfie.png";
+
 
 export default function Navbar() {
   return (
@@ -29,34 +29,7 @@ export default function Navbar() {
           </li>
         </ul>
       </nav>
-      <div className="conataine py-40 flex">
-        <div className="container pl-10">
-          <h2 className="text-4xl py-2 font-extralight">Hello, This is</h2>
-          <h1 className="text-7xl py-2 font-extralight">
-            Suryansh Singh Bisen
-          </h1>
-          <h2 className="text-4xl py-4 font-extralight">
-            Web Developer & Graphic Designer
-          </h2>
-          <button
-            className="border-solid border-2 border-red-500 rounded-md py-2 px-4 mt-4 hover:bg-red-500 hover:text-white active:bg-red-600"
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href =
-                "https://drive.google.com/file/d/1TQ1ObIeRttk_kCQzgkxZ2T5RjLSLMqlL/view?usp=sharing";
-            }}
-          >
-            Resume
-          </button>
-        </div>
-        <div className="container pl-[8rem]">
-          <img
-            src={Me}
-            alt=""
-            className="w-[30rem] rounded-full border-solid border-4 border-red-500 bg-red-200"
-          />
-        </div>
-      </div>
+      
     </div>
   );
 }
