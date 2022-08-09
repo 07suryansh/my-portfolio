683aa547106fd2973a14527f9e5e04010b5627ad
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-08-09 19:25:19 +0530

    Bottom border added to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,31 +6,31 @@ export default function Navbar() {
     <div className="">
       <nav className="font-sans text-lg flex ">
         <ul className="cursor-pointer flex ">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black ">
             Home
           </li>
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
             About
           </li>
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
             Skills
           </li>
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
             Experience
           </li>
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
             Education
           </li>
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
             Projects
           </li>
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
             Contact
           </li>
         </ul>
       </nav>
-      <div className="conataine px-20 py-40">
-        <h2 className="text-4xl py-2 font-extralight">Hello, This is</h2>
+      <div className="conataine px-20 py-40 ">
+        <h2 className="text-4xl py-2 font-extralight ">Hello, This is</h2>
         <h1 className="text-7xl py-2 font-extralight">Suryansh Singh Bisen</h1>
         <h2 className="text-4xl py-4 font-extralight">
           Web Developer & Graphic Designer
