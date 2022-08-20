9856c9bd296159ba860778c72a399632dec72acb
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-08-20 16:50:48 +0530

    Aligned icons in skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -16,63 +16,63 @@ export default function Skills() {
   return (
     <div>
       <div className="my-4">
-        <h1 className="text-4xl text-center mx-[42rem] mt-4 border-blue-500 border-b-4">
+        <h1 className="text-4xl text-center mx-[30rem] mt-4 border-blue-500 border-b-4">
           Skills
         </h1>
       </div>
           <div className="flex flex-col">
                 <div className="container m-10 flex flex-row">
-                        <div className="">
+                        <div className="px-12">
                           <img src={cpp} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">C++</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={java} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Java</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={js} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Javascript</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={react} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">ReactJs</h1>
                         </div>
                 </div>
                 <div className="container m-10 flex flex-row">
-                        <div className="">
+                        <div className="px-12">
                           <img src={python} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Python</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={html} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">HTML</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={css} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">CSS</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={tailwind} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Tailwind CSS</h1>
                         </div>
                 </div>
                 <div className="container m-10 flex flex-row">
-                        <div className="">
+                        <div className="px-12">
                           <img src={bootstrap} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Bootstrap</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={photoshop} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Photoshop</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={illustrator} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Illustrator</h1>
                         </div>
-                        <div className="">
+                        <div className="px-12">
                           <img src={figma} alt="" className="w-14"/>
-                          <h1 className="p-4">C++</h1>
+                          <h1 className="">Figma</h1>
                         </div>
                 </div>
           </div>
