aadd620fb7bfd818d14c0cff71577e5e218d8d83
Suryansh Singh Bisen <07suryansh@gmail.com>
2022-08-21 10:53:44 +0530

    given border radius to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,43 +8,43 @@ export default function Navbar() {
         <ul className="cursor-pointer flex ">
 
         <Link to="/">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black ">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:rounded-b-3xl ">
           Home
           </li>
           </Link> 
 
           <Link to="About">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:rounded-b-3xl">
            About
           </li>
           </Link> 
           
           <Link to="Skills">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:rounded-b-3xl">
           Skills
           </li>
           </Link>
 
           <Link to="Experience">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:rounded-b-3xl">
           Experience
           </li>
           </Link>
 
           <Link to="Education">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:rounded-b-3xl">
           Education
           </li>
           </Link>
 
           <Link to="Projects">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:rounded-b-3xl">
           Projects
           </li>
           </Link>
 
           <Link to="Contact">
-          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:border-b-2 border-black">
+          <li className="hover:bg-slate-200 px-6 py-4 visited:bg-slate-400 active:bg-slate-300 hover:border-solid hover:rounded-b-3xl">
           Contact
           </li>
           </Link>
